dc1a31f35f8fc72070c4de616ba44bc9b4c3c680
arturo0917123 <arthh0917@gmail.com>
2025-03-27 08:33:42 -0500

    Primer cambio

---
+++ src/components/ElonBloc.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ElonBloc.css"; // Importa los estilos
+
+const ElonBloc = () => {
+  return (
+    <div className="bloc-container">
+      <h2>Elon Musk</h2>
+      <p>
+        Elon Musk es un empresario y magnate conocido por su liderazgo en Tesla, SpaceX, Neuralink y
+        otras compañías innovadoras.
+      </p>
+      <a href="https://twitter.com/elonmusk" target="_blank" rel="noopener noreferrer">
+        Seguir en Twitter
+      </a>
+    </div>
+  );
+};
+
+export default ElonBloc;
